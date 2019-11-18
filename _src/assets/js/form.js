6363c67c5f7e616ee5fc99eadbdbf07c6ca0c3ad
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-11-18 17:05:24 +0100

    Add js to form wip

--- _src/assets/js/form.js
+++ _src/assets/js/form.js
@@ -1,13 +1,30 @@
 "use strict";
 
-const input = document.querySelector(".js-input");
+const nameEle = document.querySelector(".js-name");
+const cardNameEle = document.querySelector(".js-cardTitle");
+const jobEle = document.querySelector(".js-job");
+const cardJobEle = document.querySelector(".js-cardJob");
+const emailEle = document.querySelector(".js-email");
+const cardEmailEle = document.querySelector(".js-cardEmail");
+const empty = "";
+function changeCard() {
+  const nameValue = nameEle.value;
+  if (nameValue !== empty) {
+    cardNameEle.innerHTML = nameValue;
+  } else {
+    cardNameEle.innerHTML = "Nombre Apellido";
+  }
 
-function changeCard(event) {
-  const eleTitle = document.querySelector(".js-cardTitle");
-
-  //   debugger;
-  eleTitle.innerHTML = event.currentTarget.value;
-  const eleJob = document.querySelector(".js-cardJob");
-  eleJob.innerHTML = event.currentTarget.value;
+  const jobValue = jobEle.value;
+  if (jobValue !== empty) {
+    cardJobEle.innerHTML = jobValue;
+  } else {
+    cardJobEle.innerHTML = "Front-end developer";
+  }
+  const emailValue = emailEle.value;
+  cardEmailEle.href = `"mailto: ${emailValue}"`;
 }
-input.addEventListener("keyup", changeCard);
+
+nameEle.addEventListener("keyup", changeCard);
+jobEle.addEventListener("keyup", changeCard);
+emailEle.addEventListener("keyup", changeCard);
