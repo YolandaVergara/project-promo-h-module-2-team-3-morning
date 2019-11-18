cffe3fd27ad8518edd9e6c7da4b1c5f4d034863e
Yolanda <yv.avila@gmail.com>
2019-11-18 17:37:52 +0100

    add changes to palette colour

--- _src/assets/js/colors.js
+++ _src/assets/js/colors.js
@@ -1,26 +1,26 @@
 "use strict";
-
 const palette1 = document.getElementById("color-option1");
 const palette2 = document.getElementById("color-option2");
 const palette3 = document.getElementById("color-option3");
 
 const card = document.querySelector(".js-card");
+
 function changeColorPalette1() {
-  card.classList.remove("palette2");
-  card.classList.remove("palette3");
-  card.classList.add("palette1");
+  card.classList.remove("js-card-palette2");
+  card.classList.remove("js-card-palette3");
+  card.classList.add("js-card-palette1");
 }
 
 function changeColorPalette2() {
-  card.classList.remove("palette1");
-  card.classList.remove("palette3");
-  card.classList.add("palette2");
+  card.classList.remove("js-card-palette1");
+  card.classList.remove("js-card-palette3");
+  card.classList.add("js-card-palette2");
 }
 
 function changeColorPalette3() {
-  card.classList.remove("palette1");
-  card.classList.remove("palette2");
-  card.classList.add("palette3");
+  card.classList.remove("js-card-palette1");
+  card.classList.remove("js-card-palette2");
+  card.classList.add("js-card-palette3");
 }
 
 palette1.addEventListener("click", changeColorPalette1);
