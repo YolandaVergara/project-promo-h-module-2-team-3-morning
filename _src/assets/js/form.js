fc62b880ff725494d83ba6ee9c03960034850ed9
Azahara Martinez <azaharamaga@gmail.com>
2019-11-19 14:01:42 +0100

    Add js links wip

--- _src/assets/js/form.js
+++ _src/assets/js/form.js
@@ -6,6 +6,12 @@ const jobEle = document.querySelector(".js-job");
 const cardJobEle = document.querySelector(".js-cardJob");
 const emailEle = document.querySelector(".js-email");
 const cardEmailEle = document.querySelector(".js-cardEmail");
+const phoneEle = document.querySelector(".js-phone");
+const cardPhoneEle = document.querySelector(".js-cardPhone");
+const linkedinEle = document.querySelector(".js-linkedin");
+const cardLinkedinEle = document.querySelector(".js-cardLinkedin");
+const githubEle = document.querySelector(".js-github");
+const cardGithubEle = document.querySelector(".js-cardGithub");
 const empty = "";
 function changeCard() {
   const nameValue = nameEle.value;
@@ -21,8 +27,22 @@ function changeCard() {
   } else {
     cardJobEle.innerHTML = "Front-end developer";
   }
+
+  const phoneValue = phoneEle.value;
+  if (phoneValue !== empty) {
+    cardPhoneEle = phoneValue;
+  } else {
+    //opacidad
+  }
+
   const emailValue = emailEle.value;
   cardEmailEle.href = `mailto: ${emailValue}`;
+
+  const linkedinValue = linkedinEle.value;
+  cardLinkedinEle.href = linkedinValue;
+
+  const githubValue = githubEle.value;
+  cardGithubEle.href = githubValue;
 }
 
 nameEle.addEventListener("keyup", changeCard);
