23e1df4541bf1deca2c11713ea19652a35e55b4f
sircferrer <ferrercarballo@gmail.com>
2019-11-22 11:11:23 +0100

    add js to reset wip

--- _src/assets/js/reset.js
+++ _src/assets/js/reset.js
@@ -1,2 +1,19 @@
 "use strict";
-hola;
+const btnDelete = document.querySelector(".js-button--delete");
+
+const handleReset = function() {
+  console.log("escuchar click");
+  deleteData();
+};
+//funcion para borra los datos del form
+
+const deleteData = function() {
+  document.getElementById("js-form").reset();
+  console.log("borrar datos");
+};
+
+//funcion restaurar valores de la tarjeta
+
+//funcion para restablecer color por defecto
+
+btnDelete.addEventListener("click", handleReset);
