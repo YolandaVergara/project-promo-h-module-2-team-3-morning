230b6c44d48f0970431f4536a8ce8a6bc3d4322c
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-11-25 11:26:22 +0100

    Create object with card fields

--- _src/assets/js/reset.js
+++ _src/assets/js/reset.js
@@ -1,9 +1,9 @@
 "use strict";
 const btnDelete = document.querySelector(".js-button--delete");
-
 const handleReset = function() {
   console.log("escuchar click");
   deleteData();
+  paintCardAfterReset();
 };
 //funcion para borra los datos del form
 
@@ -11,6 +11,24 @@ const deleteData = function() {
   document.getElementById("js-form").reset();
   console.log("borrar datos");
 };
+
+//Creamos un objeto que incluye todos los campos de card-result
+
+const cardFieldsObject = {
+  name: "Nombre Apellido",
+  job: "Front-end developer",
+  // picture: "",
+  phone: "",
+  email: ""
+};
+
+function paintCardAfterReset() {
+  cardNameEle.innerHTML = cardFieldsObject.name;
+  cardJobEle.innerHTML = cardFieldsObject.job;
+  // cardEmailEle.innerHTML.href = cardFieldsObject.email;
+
+  console.log(cardEmailEle);
+}
 
 //funcion restaurar valores de la tarjeta
 
