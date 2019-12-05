57c1ba86415326532e768e295c3e372cb643289c
Azahara Martinez <azaharamaga@gmail.com>
2019-12-05 23:56:22 +0100

    Validate form when the web starts

--- _src/assets/js/form.js
+++ _src/assets/js/form.js
@@ -1,34 +1,34 @@
-"use strict";
+'use strict';
 
-const nameEle = document.querySelector(".js-name");
-const cardNameEle = document.querySelector(".js-cardTitle");
-const jobEle = document.querySelector(".js-job");
-const cardJobEle = document.querySelector(".js-cardJob");
-const emailEle = document.querySelector(".js-email");
-let cardEmailEle = document.querySelector(".js-cardEmail");
-const phoneEle = document.querySelector(".js-phone");
-let cardPhoneEle = document.querySelector(".js-cardPhone");
-const linkedinEle = document.querySelector(".js-linkedin");
-let cardLinkedinEle = document.querySelector(".js-cardLinkedin");
-const githubEle = document.querySelector(".js-github");
-let cardGithubEle = document.querySelector(".js-cardGithub");
-const liElePhone = document.querySelector(".itemPhone");
-const liEleMail = document.querySelector(".itemEmail");
-const liEleLinkedin = document.querySelector(".itemLinkedin");
-const liEleGithub = document.querySelector(".itemGithub");
-const empty = "";
-const cardImage = document.querySelector(".card__image");
-const imgPreview = document.querySelector(".js__profile-preview");
-const submitEl = document.querySelector(".js-button");
-const shareEle = document.querySelector(".js-shareResult");
+const nameEle = document.querySelector('.js-name');
+const cardNameEle = document.querySelector('.js-cardTitle');
+const jobEle = document.querySelector('.js-job');
+const cardJobEle = document.querySelector('.js-cardJob');
+const emailEle = document.querySelector('.js-email');
+let cardEmailEle = document.querySelector('.js-cardEmail');
+const phoneEle = document.querySelector('.js-phone');
+let cardPhoneEle = document.querySelector('.js-cardPhone');
+const linkedinEle = document.querySelector('.js-linkedin');
+let cardLinkedinEle = document.querySelector('.js-cardLinkedin');
+const githubEle = document.querySelector('.js-github');
+let cardGithubEle = document.querySelector('.js-cardGithub');
+const liElePhone = document.querySelector('.itemPhone');
+const liEleMail = document.querySelector('.itemEmail');
+const liEleLinkedin = document.querySelector('.itemLinkedin');
+const liEleGithub = document.querySelector('.itemGithub');
+const empty = '';
+const cardImage = document.querySelector('.card__image');
+const imgPreview = document.querySelector('.js__profile-preview');
+const submitEl = document.querySelector('.js-button');
+const shareEle = document.querySelector('.js-shareResult');
 
 const cardFieldsDefaultValues = {
-  name: "Nombre Apellido",
-  job: "Front-end developer",
-  phone: "tel:",
-  email: "mailto:",
-  linkedin: "",
-  github: ""
+  name: 'Nombre Apellido',
+  job: 'Front-end developer',
+  phone: 'tel:',
+  email: 'mailto:',
+  linkedin: '',
+  github: '',
 };
 
 function changeCard() {
@@ -48,56 +48,61 @@ function changeCard() {
 
   let phoneValue = phoneEle.value;
   if (phoneValue !== empty) {
-    liElePhone.classList.remove("opacity");
+    liElePhone.classList.remove('opacity');
     cardPhoneEle.href = `tel:${phoneValue}`;
   } else {
-    liElePhone.classList.add("opacity");
+    liElePhone.classList.add('opacity');
   }
 
   let emailValue = emailEle.value;
 
   if (emailValue !== empty) {
-    liEleMail.classList.remove("opacity");
+    liEleMail.classList.remove('opacity');
     cardEmailEle.href = `mailto:${emailValue}`;
   } else {
-    liEleMail.classList.add("opacity");
+    liEleMail.classList.add('opacity');
   }
 
   let linkedinValue = linkedinEle.value;
   if (linkedinValue !== empty) {
-    liEleLinkedin.classList.remove("opacity");
+    liEleLinkedin.classList.remove('opacity');
     cardLinkedinEle.href = linkedinValue;
   } else {
-    liEleLinkedin.classList.add("opacity");
+    liEleLinkedin.classList.add('opacity');
   }
 
   let githubValue = githubEle.value;
   if (githubValue !== empty) {
-    liEleGithub.classList.remove("opacity");
+    liEleGithub.classList.remove('opacity');
     cardGithubEle.href = githubValue;
   } else {
-    liEleGithub.classList.add("opacity");
+    liEleGithub.classList.add('opacity');
   }
   setToLocalStorage();
 }
 
-nameEle.addEventListener("keyup", changeCard);
-jobEle.addEventListener("keyup", changeCard);
-emailEle.addEventListener("keyup", changeCard);
-phoneEle.addEventListener("keyup", changeCard);
-linkedinEle.addEventListener("keyup", changeCard);
-githubEle.addEventListener("keyup", changeCard);
+nameEle.addEventListener('keyup', changeCard);
+jobEle.addEventListener('keyup', changeCard);
+emailEle.addEventListener('keyup', changeCard);
+phoneEle.addEventListener('keyup', changeCard);
+linkedinEle.addEventListener('keyup', changeCard);
+githubEle.addEventListener('keyup', changeCard);
 
 //Validamos el formulario y añadimos la clase "active" cuando el formulario está cumplimentado
-const containerForm = document.querySelector(".js-containerForm");
+const containerForm = document.querySelector('.js-containerForm');
 const validation = function(event) {
   event.preventDefault();
+  activateShareButton();
+};
+
+// Activar el boton de compartir solo si el formulario esta completo
+const activateShareButton = function() {
   const check = containerForm.checkValidity();
   if (check === true) {
-    submitEl.classList.add("share__button--active");
+    submitEl.classList.add('share__button--active');
   } else {
-    submitEl.classList.remove("share__button--active");
+    submitEl.classList.remove('share__button--active');
   }
 };
 
-containerForm.addEventListener("change", validation);
+containerForm.addEventListener('change', validation);
--- _src/assets/js/localStorage.js
+++ _src/assets/js/localStorage.js
@@ -32,7 +32,7 @@ const getFromLocalStorage = () => {
     //parseamos el dato del valor del checked y lo asignamos a una constante que posteriormente meteremos interpolando en el queryselector
     const selectedPalette = userData.colour;
     document.querySelector(`#color-option${selectedPalette}`).checked = true;
-
+    activateShareButton();
     //llamamos a las funciones que ya tenemos definidas en otros partials, para que haga el hilo de, cambiar la tarjeta, volver a cargar la imagen previsualizada y vuelva a cambiar el color de la paleta seleccionada
     changeCard();
     previewImage();
